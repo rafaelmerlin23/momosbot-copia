0753a888c81a6cefdd0ec2a579cea13045957537
rafael <rafaelmerlinprieto@gmail.com>
2024-09-06 13:55:46 -0600

    momos bot

--- App/resources/credentials.py
+++
@@ -1,6 +0,0 @@
-twitter_credentials = {    "api_key" : "OYCVWF6UJqSgrQtGU5OLXbqki",
-     "api_key_secret" : "KhUyejRbpWEU2SzhlX1xw62bTEbf4HzTCSIup1EYIWHylwAGDL",
-     "bearer_token" : "AAAAAAAAAAAAAAAAAAAAALrgvgEAAAAAXF6vMNn6ChUzpp6KQ7SomvjSti0%3Dy7QzbslPACMfMbc7eUDe55gvT1GvNTqtYkzlE4yzLKZdv2BJmn",
-     "access_token" : "1831425960538673152-vvpePMczfVvX47Nz9EhHQV6uaxFuza",
-     "access_token_secret" : "EaLHwiWHySlTzTuFh7qSAhY55aCNi4u0jXeRipQn6OvVx"
-}
